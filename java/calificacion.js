de7145769c97e0156e53185a57243072426df32a
genesis-miranda <165221847+genesis-miranda@users.noreply.github.com>
2024-09-24 21:16:30 -0300

    Add files via upload

---
+++ java/calificacion.js
@@ -0,0 +1,6 @@
+let calificacion = 9;
+if (calificacion>=6 && calificacion < 10){
+    console.log("estas aprobado")
+}else{
+    console.log("estas desaprobado")
+}
---
+++ java/numero-par-impar.js
@@ -0,0 +1,6 @@
+let numero = 12
+if (numero%2 == 0 ) {
+    console.log ("el numero es par")
+}else{
+    console.log("el numero es impar")
+}
